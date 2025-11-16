3407771ffdacbda201f14c50af3b57683dd1ebc8
blood bank <solankimeet0512@gmail.com>
2025-11-16 18:42:46 +0530

    16-11-25

---
+++ Main Assignment/Core JAVA Assignment/Lab Exersice/lab2.java
@@ -0,0 +1,20 @@
+package assignment;
+
+public class lab2 {
+
+	public static void main(String[] args) {
+        int age = 25;
+        float salary = 1000.30f;
+        double height = 4.9;
+        char grade = 'A';
+        boolean isEmployee = true;
+        String name = "Meet";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Employee: " + isEmployee);
+    }
+}
